a2c8d0c3cce54fdb1934cd5aa64e8d7cf142f4b2
Everson Moura <everson.moura@consulting-for.edenred.com>
2020-08-07 17:26:24 -0300

    'init'

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("#clickme").click(function () {
+        $("#lancax").animate({
+            opacity: 0.25,
+            left: "+=50",
+            height: "toggle"
+        }, 5000, function () {
+            // Animation complete.
+        });
+    });
+
+  
+
+});
